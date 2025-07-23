bec34a4583098e91d518033e5dd943bc211687ba
BhutSipho <masangosipho007@gmail.com>
2025-07-23 16:11:58 +0200

    Registering

--- my-flask-app-vite/src/services/api.ts
+++ my-flask-app-vite/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance with default config
 const api = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
